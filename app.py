209e4d146a6489848390143894de9872303c4256
Gutha Sai Mukesh Reddy <78792429+saimukeshreddygutha@users.noreply.github.com>
2022-03-28 22:34:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request, jsonify
 
-from bot import response
+from chat import response
 
 app = Flask(__name__)
 
 @app.get('/')
 def index_get():
-    return render_template(base.html)
+    return render_template("base.html")
 @app.post("/predict")
 def predict():
     t = request.get_json().get("message")
-    response = response("t")
-    message = {'answer': response}
+    responset = response(t)
+    message = {'answer': responset}
     return jsonify(message)
 
 
